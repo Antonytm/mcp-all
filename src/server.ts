f441519a572abf47afd879fb65b9f3782210583b
Antonytm <antontishchenko@gmail.com>
2025-09-28 18:27:01 +0300

    feat: version+1

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ export async function getServer(config: Config): Promise<McpServer> {
     const server = new McpServer({
         name: `Universal MCP Server`,
         description: "Model Context Protocol Server for using all MCP servers",
-        version: "0.1.18",
+        version: "0.1.19",
     });
 
     if (!fs.existsSync(config.mcpConfigPath)) {
